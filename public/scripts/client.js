594f771748246af9d6e9ec4b4bd7b49cf7df2edc
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2022-12-12 14:25:19 -0800

    implement IIFE

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,44 +1,44 @@
 /*
 * Client side Javascript
 */
+(function ($) {
+  $(document).ready(function () {
 
-$(document).ready(function() {
+    $('.tweet-submission').on('submit', postTweets);
+    loadTweets();
 
-  $('.tweet-submission').on('submit', postTweets);
-  loadTweets();
+    // Stretch project for scroll button. Missing logic to hide it when clicked.Keeps registering scroll back up and then redisplaying.
+    $('.scroll-top-button, .error').hide();
+    $('.nav-button, .scroll-top-button').on('click', focusTextArea);
 
-  // Stretch project for scroll button. Missing logic to hide it when clicked.Keeps registering scroll back up and then redisplaying.
-  $('.scroll-top-button, .error').hide();
-  $('.nav-button, .scroll-top-button').on('click', focusTextArea);
+    // Ideally add logic to stop scroll being registered, preventDefault?Conditional?TBC
+    $(window).on('scroll', function () {
+      $('.scroll-top-button').fadeIn();
+    });
 
-  // Ideally add logic to stop scroll being registered, preventDefault?Conditional?TBC
-  $(window).on('scroll', function() {
-    $('.scroll-top-button').fadeIn();
+    if ($('.new-tweet').is(':visible')) {
+      $('#new-tweet-content').focus();
+    }
+
   });
 
-  if ($('.new-tweet').is(':visible')) {
-    $('#new-tweet-content').focus();
-  }
+  //HELPER FUNCTIONS----------------------------------------------------------------------------------------------------------------------------
+  /**
+   * Prevents cross site scripting
+   * @param {*} str The content being submitted
+   * @returns A string that allows the tweet to be submitted without consequence
+   */
+  let crossSiteScripting = function (str) {
+    return str.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  };
 
-});
-
-//HELPER FUNCTIONS----------------------------------------------------------------------------------------------------------------------------
-/**
- * Prevents cross site scripting
- * @param {*} str The content being submitted
- * @returns A string that allows the tweet to be submitted without consequence
- */
-let crossSiteScripting = function(str) {
-  return str.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-};
-
-/**
- * Returns HTML text using key value pairs to populate each individual tweet in the log
- * @param {*} tweet The tweet object
- * @returns HTML text to be inserted into the HTML article
- */
-const createTweetElement = function(tweet) {
-  return `
+  /**
+   * Returns HTML text using key value pairs to populate each individual tweet in the log
+   * @param {*} tweet The tweet object
+   * @returns HTML text to be inserted into the HTML article
+   */
+  const createTweetElement = function (tweet) {
+    return `
   <article>
     <header class ='tweet-header'> 
       <img class ='log-avatar' src='${tweet.user.avatars}'> 
@@ -59,80 +59,81 @@ const createTweetElement = function(tweet) {
 
   </article>
   `;
-};
+  };
 
 
-/**
- * Takes in an array of objects and parses each tweet through the createElement function
- * @param {*} tweets The array of tweet objects
- */
-const renderTweets = function(tweets) {
-  const $container = $('.log-container');
-  $container.empty(); // To empty container so its starting with a fresh slate each time
-  for (let tweet of tweets) {
-    $container.prepend(createTweetElement(tweet)); // Prepend for reverse order
-  }
-};
+  /**
+   * Takes in an array of objects and parses each tweet through the createElement function
+   * @param {*} tweets The array of tweet objects
+   */
+  const renderTweets = function (tweets) {
+    const $container = $('.log-container');
+    $container.empty(); // To empty container so its starting with a fresh slate each time
+    for (let tweet of tweets) {
+      $container.prepend(createTweetElement(tweet)); // Prepend for reverse order
+    }
+  };
 
 
-/**
- * Sending tweets to server and blocking redirect
- * @param {*} event In this case, on Click
- */
-const postTweets = function(event) {
-  event.preventDefault();
+  /**
+   * Sending tweets to server and blocking redirect
+   * @param {*} event In this case, on Click
+   */
+  const postTweets = function (event) {
+    event.preventDefault();
 
-  const inputData = $('#new-tweet-content').val();
-  if (inputData === "" || inputData === null) {
-    $('.error').slideUp('fast');
-    $('.error').html(`<strong>You got nothing to say?</strong>`).slideDown('fast');
-  } else if (inputData.length > 140) {
-    $('.error').slideUp('fast');
-    $('.error').html(`<strong>Ok now you're saying too much.</strong>`).slideDown('fast');
-  } else {
-    $('.error').slideUp('fast');
-    const form = $(this).serialize();
-    $.post('/tweets', form)
-      .then(() => {
-        $('#new-tweet-content').val('');
-        $('.counter').html('140');
-        loadTweets(); // To add the new tweet to the top of the page after clicking submit
+    const inputData = $('#new-tweet-content').val();
+    if (inputData === "" || inputData === null) {
+      $('.error').slideUp('fast');
+      $('.error').html(`<strong>You got nothing to say?</strong>`).slideDown('fast');
+    } else if (inputData.length > 140) {
+      $('.error').slideUp('fast');
+      $('.error').html(`<strong>Ok now you're saying too much.</strong>`).slideDown('fast');
+    } else {
+      $('.error').slideUp('fast');
+      const form = $(this).serialize();
+      $.post('/tweets', form)
+        .then(() => {
+          $('#new-tweet-content').val('');
+          $('.counter').html('140');
+          loadTweets(); // To add the new tweet to the top of the page after clicking submit
+        })
+        .catch((e) => {
+          $('.error').slideUp('fast');
+          $('.error').html(`<strong>Server POST Error: ${e}.</strong>`).slideDown('fast');
+        });
+    }
+  };
+
+  /**
+   * loadTweets shows all tweets when home page is loaded. Called in document ready below
+   */
+  const loadTweets = function () {
+    $.get('/tweets')
+      .then(tweets => {
+        renderTweets(tweets); // Accessing and rendering the array of objects to be parsed and displayed
       })
       .catch((e) => {
         $('.error').slideUp('fast');
-        $('.error').html(`<strong>Server POST Error: ${e}.</strong>`).slideDown('fast');
+        $('.error').html(`<strong>Server GET Error: ${e}.</strong>`).slideDown('fast');
       });
-  }
-};
-
-/**
- * loadTweets shows all tweets when home page is loaded. Called in document ready below
- */
-const loadTweets = function() {
-  $.get('/tweets')
-    .then(tweets => {
-      renderTweets(tweets); // Accessing and rendering the array of objects to be parsed and displayed
-    })
-    .catch((e) => {
-      $('.error').slideUp('fast');
-      $('.error').html(`<strong>Server GET Error: ${e}.</strong>`).slideDown('fast');
-    });
-};
+  };
 
 
-/**
- * STRETCH -Current bug that hides scroll button once button is clicked. TBC.
- */
-const focusTextArea = function() {
-  $('#new-tweet-content').focus();
-  let position = $('.new-tweet').offset().top;
-  let navHeight = $('nav').outerHeight();
-  let headerHeight = $('header').outerHeight();
-  $('html').animate({
-    scrollTop: position - navHeight - headerHeight
-  }, '1000');
-};
+  /**
+   * STRETCH -Current bug that hides scroll button once button is clicked. TBC.
+   */
+  const focusTextArea = function () {
+    $('#new-tweet-content').focus();
+    let position = $('.new-tweet').offset().top;
+    let navHeight = $('nav').outerHeight();
+    let headerHeight = $('header').outerHeight();
+    $('html').animate({
+      scrollTop: position - navHeight - headerHeight
+    }, '1000');
+  };
 
+})(jQuery);
 
 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,33 +1,35 @@
 //Character count helper function
+(function ($) {
+  $(document).ready(function () {
 
-$(document).ready(function () {
+    $('#new-tweet-content').on('input', onInput);
 
-  $('#new-tweet-content').on('input', onInput);
+  });
 
-});
+  /**
+   * On text input, updates the character counter. Add/removes classes which turns text black/red based on counter in relation to 140.
+   * @param {*} event The key pressed
+   * @returns An updated value based on input length
+   */
+  const onInput = function (event) {
 
-/**
- * On text input, updates the character counter. Add/removes classes which turns text black/red based on counter in relation to 140.
- * @param {*} event The key pressed
- * @returns An updated value based on input length
- */
-const onInput = function (event) {
+    let $input = $(this);
+    let currentLength = $input.val().length;
+    let remaining = 140 - currentLength;
 
-  let $input = $(this);
-  let currentLength = $input.val().length;
-  let remaining = 140 - currentLength;
+    //Traverse up and then back down to find .counter
+    let $parent = $input.parent();
+    let $counter = $parent.find('.counter');
+    $counter.each(function () {
+      $counter.text(remaining);
 
-  //Traverse up and then back down to find .counter
-  let $parent = $input.parent();
-  let $counter = $parent.find('.counter');
-  $counter.each(function () {
-    $counter.text(remaining);
+      if (remaining < 0) {
+        $counter.addClass('negative-count');
+      } else {
+        $counter.removeClass('negative-count')
+      }
+    })
+    return;
+  };
 
-    if (remaining < 0) {
-      $counter.addClass('negative-count');
-    } else {
-      $counter.removeClass('negative-count')
-    }
-  })
-  return;
-};
+})(jQuery);
